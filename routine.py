ad82bb425e43be1b39ca5e9e1a77772a45b16a6e
Radon8606 <rohinnayar5@gmail.com>
2023-10-15 22:10:10 +0100

    Updated main script with notes to access and store data, formatted the excel log

--- routine.py
+++ routine.py
@@ -1,3 +1,9 @@
+from functools import reduce
+
+# Concept is to have this record my workouts and perhaps write it to a file
+# Also want to plot a basic activity graph to track and keep record of 
+# the number of swim, gym, cycling and running sessions completed 
+# during the week
 
 class Routine:
 
@@ -10,14 +16,25 @@ class Routine:
         'Saturday': [],
         'Sunday': []}
     
-    def __init__(self, date, sport, duration):
+    def __init__(self, date, sport, duration, NumberOfSessions):
         self.date = date
         self.sport = sport
         self.duration = duration
+        self.NumberOfSessions = NumberOfSessions
     
     def __repr__(self):
         return "On {date} you have {sport} for {duration}!".format(date = self.date, sport = self.sport, duration = self.duration)
     
-    # Allocate the parameters to the daily routine lists
-    def setActivity(self, data):
-        data[self.date] = [self.sport, self.duration]
+    # Allocate the parameters to the data dictionary
+    def setActivity(self,data):
+        data[self.date] = [self.sport, self.duration, self.NumberOfSessions]
+
+    # Store entries to a text file or csv file 
+    
+    # Can use a dataframe to store and access data too
+
+    # Retrieve data depending on input dates
+    def getActivity(self):
+
+        
+
