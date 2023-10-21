11184390b014735b284b6d678c70b23c91916c45
Radon8606 <rohinnayar5@gmail.com>
2023-10-21 17:39:07 +0100

    Created csv file to import data, rewriting routine script to plot and give analysis of data

--- routine.py
+++ routine.py
@@ -1,40 +1,40 @@
 from functools import reduce
+import pandas as pd
 
 # Concept is to have this record my workouts and perhaps write it to a file
 # Also want to plot a basic activity graph to track and keep record of 
 # the number of swim, gym, cycling and running sessions completed 
 # during the week
 
-class Routine:
+test = pd.read_csv("Data/Records.csv")
+print(test.head(5))
+# class Routine:
 
-    data = {
-        'Monday': [],
-        'Tuesday': [],
-        'Wednesday': [],
-        'Thursday': [],
-        'Friday': [],
-        'Saturday': [],
-        'Sunday': []}
+#     data = {
+#         'Monday': [],
+#         'Tuesday': [],
+#         'Wednesday': [],
+#         'Thursday': [],
+#         'Friday': [],
+#         'Saturday': [],
+#         'Sunday': []}
     
-    def __init__(self, date, sport, duration, NumberOfSessions):
-        self.date = date
-        self.sport = sport
-        self.duration = duration
-        self.NumberOfSessions = NumberOfSessions
+#     def __init__(self, date, sport, duration, NumberOfSessions):
+#         self.date = date
+#         self.sport = sport
+#         self.duration = duration
+#         self.NumberOfSessions = NumberOfSessions
     
-    def __repr__(self):
-        return "On {date} you have {sport} for {duration}!".format(date = self.date, sport = self.sport, duration = self.duration)
+#     def __repr__(self):
+#         return "On {date} you have {sport} for {duration}!".format(date = self.date, sport = self.sport, duration = self.duration)
     
-    # Allocate the parameters to the data dictionary
-    def setActivity(self,data):
-        data[self.date] = [self.sport, self.duration, self.NumberOfSessions]
+#     # Allocate the parameters to the data dictionary
+#     def setActivity(self,data):
+#         data[self.date] = [self.sport, self.duration, self.NumberOfSessions]
 
-    # Store entries to a text file or csv file 
+#     # Store entries to a text file or csv file 
     
-    # Can use a dataframe to store and access data too
+#     # Can use a dataframe to store and access data too
 
-    # Retrieve data depending on input dates
-    def getActivity(self):
-
-        
-
+#     # Retrieve data depending on input dates
+#     def getActivity(self):
