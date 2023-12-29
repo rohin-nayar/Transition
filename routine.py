b42676e0bbdee998c468518a407b88c6172f82b1
Radon8606 <rohinnayar5@gmail.com>
2023-12-29 19:54:45 +0000

    Edited data and added seaborn multi-grid plots

--- routine.py
+++ routine.py
@@ -6,27 +6,37 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from matplotlib.dates import MonthLocator
 
-#Enables control over what parameters to plot
-#to understand specifics such as consistency for duration of swims
-
 class DataAnalyser:
     
     def __init__(self, dataRecords):
         self.dataRecords = dataRecords
 
     def loadData(self):
-        self.data = pd.DataFrame(pd.read_csv(self.dataRecords))
+        self.data = pd.read_csv(self.dataRecords)
         print(self.data.head(5))
         self.data['Date'] = pd.to_datetime(self.data['Date'])
 
-    def plotData(self):
-        fig, ax = plt.subplots()
+    def plotData(self, xval='Date', yvals=['Body Weight (kg)', 'Duration (min)']):
         sns.set_theme()
-        sns.lineplot(x='Date', y='Body Weight (kg)', data=self.data, marker='o', color='b')  
-        ax.xaxis.set_major_locator(MonthLocator(interval=1))
-        plt.xlabel('Date')
-        plt.ylabel('Body Weight (kg)')
-        plt.title('Body Weight Over Time')
+        sns.set_style("whitegrid")
+
+        # Create subplots
+        fig, axes = plt.subplots(nrows=len(yvals), figsize=(12, 7))
+
+        # Plot each set of data on its respective subplot
+        for idx, yval in enumerate(yvals):
+            if yval == 'Duration (min)':
+                # Use bar plot for Duration
+                sns.barplot(x=xval, y=yval, data=self.data, color='b', ax=axes[idx])
+                # Set x-axis major ticks at monthly intervals
+                axes[idx].xaxis.set_major_locator(plt.MaxNLocator(nbins=12))
+
+            else:
+                # Use line plot for other variables
+                sns.lineplot(x=xval, y=yval, data=self.data, marker='o', color='g', ax=axes[idx])
+                axes[idx].set_xlabel(xval)
+                axes[idx].set_ylabel(yval)
+
         plt.tight_layout()
         plt.show()
 
