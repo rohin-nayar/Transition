3a0d9bbc8dc3e6d90be2f303eaba6be6402c0910
Radon8606 <rohinnayar5@gmail.com>
2024-01-15 21:01:28 +0000

    Start of system design structure - static methods

--- routine.py
+++ routine.py
@@ -8,15 +8,28 @@ from matplotlib.dates import MonthLocator
 
 class DataAnalyser:
     
-    def __init__(self, dataRecords):
-        self.dataRecords = dataRecords
+    def __init__(self, dataRecords, features):
 
-    def loadData(self):
+        self.dataRecords = dataRecords
+        self.features = features
+        self.__init_loadData()
+        self.__init_plotData()
+    
+    @staticmethod
+    def check_features(self):
+        dataCheck = pd.read_csv(self.dataRecords)
+        count = 0
+        for item in self.features:
+            if item in dataCheck.columns:
+                count += 1
+        return print("Number of Features to plot: " + str(count))
+
+    def __init_loadData(self):
         self.data = pd.read_csv(self.dataRecords)
         print(self.data.head(5))
         self.data['Date'] = pd.to_datetime(self.data['Date'])
 
-    def plotData(self, xval='Date', yvals=['Body Weight (kg)', 'Duration (min)']):
+    def __init_plotData(self, xval='Date', yvals=['Body Weight (kg)', 'Duration (min)']):
         sns.set_theme()
         sns.set_style("whitegrid")
 
@@ -40,7 +53,5 @@ class DataAnalyser:
         plt.tight_layout()
         plt.show()
 
-# Example for testing
-example = DataAnalyser(dataRecords="Data/Records.csv")
-example.loadData()
-example.plotData()
+if __name__ == "__main__":
+    rohin = DataAnalyser(dataRecords="Data/Records_csv.csv",features=None)
